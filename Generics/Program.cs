ba85a376cd0a22eecc2beaeae63a446ada2ea103
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-16 02:43:38 +0530

    Add: Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+        }
+    
+
+        public static void toPrint<T> (T[]intputArray)
+            {
+            foreach (var element in intputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("_____________");
+        }
+    }
+}
